1f1bb6ce1c25acbc9ddbc650a4975d8772ad7c32
bear <ungnam0509@gmail.com>
2024-04-11 16:52:49 +0900

    remove: 사용하지 않는 컴포넌트 및 화면 삭제

--- screens/HomeStack.js
+++ screens/HomeStack.js
@@ -1,8 +1,6 @@
 import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import FeedScreen from "./FeedScreen";
 import ProfileScreen from "./ProfileScreen";
-import PostScreen from "./PostScreen";
 import CalendarScreen from "./CalendarScreen";
 
 const Stack = createNativeStackNavigator();
@@ -10,10 +8,8 @@ const Stack = createNativeStackNavigator();
 function HomeStack() {
   return (
     <Stack.Navigator>
-      <Stack.Screen name="Calendar" component={CalendarScreen} />
-      <Stack.Screen name="Feed" component={FeedScreen} />
-      <Stack.Screen name="Profile" component={ProfileScreen} />
-      <Stack.Screen name="Post" component={PostScreen} />
+      <Stack.Screen name="HealthMate ··· 일정" component={CalendarScreen} />
+      <Stack.Screen name="HealthMate ··· 내 프로필" component={ProfileScreen} />
     </Stack.Navigator>
   );
 }
--- screens/MyProfileStack.js
+++ screens/MyProfileStack.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import MyProfileScreen from "./MyProfileScreen";
-import PostScreen from "./PostScreen";
 import MemberDetailScreen from "./MemberDetailScreen";
 
 const Stack = createNativeStackNavigator();
@@ -10,7 +9,6 @@ function MyProfileStack() {
   return (
     <Stack.Navigator>
       <Stack.Screen name="MyProfile" component={MyProfileScreen} />
-      <Stack.Screen name="Post" component={PostScreen} />
       <Stack.Screen name="MemberDetail" component={MemberDetailScreen} />
     </Stack.Navigator>
   );
