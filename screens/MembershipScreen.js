b241d33fa3651c28542b5dd6e0d3f639385b9180
BEAR <ungnam0509@gmail.com>
2024-05-02 15:21:53 +0900

    feat: 종료날짜 구현 및 횟수연장/요일&시간변경 기능 구현

--- screens/MembershipScreen.js
+++ screens/MembershipScreen.js
@@ -1,5 +1,13 @@
 import React, { useEffect, useState } from "react";
-import { Pressable, StyleSheet, View, Text } from "react-native";
+import {
+  Pressable,
+  StyleSheet,
+  View,
+  Text,
+  Modal,
+  TextInput,
+} from "react-native";
+import { CheckBox } from "react-native-elements";
 import { useNavigation, useRoute } from "@react-navigation/native";
 import { useUserContext } from "../contexts/UserContext";
 import IconRightButton from "../components/IconRightButton";
@@ -8,32 +16,70 @@ import {
   createSchedulesWithMembership,
   removeSchedulesWithMember,
 } from "../lib/schedules";
-import { format } from "date-fns";
+import { addDays, format } from "date-fns";
+import BorderedInput from "../components/BorderedInput";
 
 function MembershipScreen() {
+  const [showFirst, setShowFirst] = useState(false);
+  const [showSecond, setShowSecond] = useState(false);
   const navigation = useNavigation();
   const route = useRoute();
   const { user } = useUserContext();
   const [membership, setMembership] = useState({});
+  const [membershipCount, setMembershipCount] = useState(0);
+  const [membershipDays, setMembershipDays] = useState({
+    월: {
+      checked: false,
+      startHours: null,
+      startMinutes: null,
+      endHours: null,
+      endMinutes: null,
+    },
+    화: {
+      checked: false,
+      startHours: null,
+      startMinutes: null,
+      endHours: null,
+      endMinutes: null,
+    },
+    수: {
+      checked: false,
+      startHours: null,
+      startMinutes: null,
+      endHours: null,
+      endMinutes: null,
+    },
+    목: {
+      checked: false,
+      startHours: null,
+      startMinutes: null,
+      endHours: null,
+      endMinutes: null,
+    },
+    금: {
+      checked: false,
+      startHours: null,
+      startMinutes: null,
+      endHours: null,
+      endMinutes: null,
+    },
+    토: {
+      checked: false,
+      startHours: null,
+      startMinutes: null,
+      endHours: null,
+      endMinutes: null,
+    },
+    일: {
+      checked: false,
+      startHours: null,
+      startMinutes: null,
+      endHours: null,
+      endMinutes: null,
+    },
+  });
 
   const { memberId } = route.params;
-
-  // const daysNumber = {
-  //   일: 0,
-  //   월: 1,
-  //   화: 2,
-  //   수: 3,
-  //   목: 4,
-  //   금: 5,
-  //   토: 6,
-  // };
-  // const checkedDays = [];
-
-  // Object.keys(membership.days).forEach((v) => {
-  //   if (Object.keys(daysNumber).includes(v)) {
-
-  //   }
-  // })
 
   useEffect(() => {
     navigation.setOptions({
@@ -53,8 +99,11 @@ function MembershipScreen() {
     // 회원권에 status라는 속성을 두어
     // 활성화 상태: active, 중단된 상태: paused, 만료된 상태: expired로 설정
     console.log("일시중지");
-    updateMembership(membership.id, { status: "paused" });
-    removeSchedulesWithMember(user.id, memberId);
+    updateMembership(membership.id, { status: "paused" }).then(() => {
+      getMembership(user.id, memberId)
+        .then(setMembership)
+        .then(() => removeSchedulesWithMember(user.id, memberId));
+    });
   };
 
   const onPressResume = () => {
@@ -71,12 +120,84 @@ function MembershipScreen() {
 
   const onPressExtend = () => {
     // 현재 잔여횟수를 보여주고 연장할 횟수를 입력하도록 모달을 보여줌
-    console.log("횟수연장");
+    setShowFirst(true);
+  };
+
+  const onSaveExtend = () => {
+    updateMembership(membership.id, {
+      count: +membership.count + +membershipCount,
+      remaining: +membership.remaining + +membershipCount,
+    }).then(() => {
+      // TODO: createSchedulesWithMembership를 써야 하는데... startDate가 기준이 아닌 endDate가 기준이 되야 함
+      createSchedulesWithMembership({
+        ...membership,
+        count: membershipCount,
+        startDate: format(
+          addDays(new Date(membership.endDate), 1),
+          "yyyy-MM-dd"
+        ),
+      });
+    });
+    setMembershipCount(0);
+    setShowFirst(false);
+  };
+
+  const onCloseExtend = () => {
+    setMembershipCount(0);
+    setShowFirst(false);
   };
 
   const onPressChange = () => {
     // 현재 요일/시간 체크 및 설정 상황을 보여주고 직접 입력하도록 모달을 보여줌
-    console.log("요일/시간변경");
+    const days = { ...membershipDays };
+    Object.keys(membership.days).forEach((day) => {
+      days[day] = {
+        checked: true,
+        startHours: membership.days[day].startTime.split(":")[0],
+        startMinutes: membership.days[day].startTime.split(":")[1],
+        endHours: membership.days[day].endTime.split(":")[0],
+        endMinutes: membership.days[day].endTime.split(":")[1],
+      };
+    });
+    setMembershipDays(days);
+    setShowSecond(true);
+  };
+
+  const onSaveChange = () => {
+    removeSchedulesWithMember(user.id, memberId)
+      .then(() => {
+        const formatDays = {};
+        Object.entries(membershipDays).forEach(([day, data]) => {
+          if (data.checked) {
+            formatDays[day] = {
+              startTime: `${data.startHours}:${data.startMinutes}`,
+              endTime: `${data.endHours}:${data.endMinutes}`,
+            };
+          }
+        });
+        updateMembership(membership.id, { days: formatDays });
+      })
+      .then(() => {
+        getMembership(user.id, memberId)
+          .then(setMembership)
+          .then(() => createSchedulesWithMembership(membership));
+      });
+    setShowSecond(false);
+  };
+
+  const onCloseChange = () => {
+    const days = { ...membershipDays };
+    Object.keys(membership.days).forEach((day) => {
+      days[day] = {
+        checked: true,
+        startHours: membership.days[day].startTime.split(":")[0],
+        startMinutes: membership.days[day].startTime.split(":")[1],
+        endHours: membership.days[day].endTime.split(":")[0],
+        endMinutes: membership.days[day].endTime.split(":")[1],
+      };
+    });
+    setMembershipDays(days);
+    setShowSecond(false);
   };
 
   const showMembershipDays = () => {
@@ -107,6 +228,9 @@ function MembershipScreen() {
         <Text style={styles.itemText}>시작일자: {membership?.startDate}</Text>
       </View>
       <View style={styles.item}>
+        <Text style={styles.itemText}>종료일자: {membership?.endDate}</Text>
+      </View>
+      <View style={styles.item}>
         <Text style={styles.itemText}>등록횟수: {membership?.count}</Text>
       </View>
       <View style={styles.item}>
@@ -128,20 +252,215 @@ function MembershipScreen() {
             {membership?.status === "active" ? "일시중지" : "재개하기"}
           </Text>
         </Pressable>
-        <Pressable
-          style={styles.button}
-          android_ripple={{ color: "#ededed" }}
-          onPress={onPressExtend}
+        {membership?.status === "active" ? (
+          <Pressable
+            style={styles.button}
+            android_ripple={{ color: "#ededed" }}
+            onPress={onPressExtend}
+          >
+            <Text>횟수연장</Text>
+          </Pressable>
+        ) : null}
+        <Modal
+          visible={showFirst}
+          animationType="fade"
+          transparent={true}
+          onRequestClose={onCloseExtend}
         >
-          <Text>횟수연장</Text>
-        </Pressable>
-        <Pressable
-          style={styles.button}
-          android_ripple={{ color: "#ededed" }}
-          onPress={onPressChange}
+          <View style={styles.modalContainer}>
+            <View style={styles.modalContent}>
+              <View
+                style={{
+                  position: "absolute",
+                  top: 15,
+                  left: 22,
+                }}
+              >
+                <Text style={{ fontSize: 24 }}>횟수연장</Text>
+              </View>
+              <View style={{ flexDirection: "row", alignItems: "center" }}>
+                <BorderedInput
+                  placeholder="연장할 횟수를 입력하세요."
+                  value={membershipCount}
+                  onChangeText={setMembershipCount}
+                  keyboardType="number-pad"
+                />
+              </View>
+              <View
+                style={{
+                  flexDirection: "row",
+                  position: "absolute",
+                  bottom: 10,
+                  right: 10,
+                }}
+              >
+                <Pressable onPress={onSaveExtend} style={{ padding: 10 }}>
+                  <Text style={{ fontSize: 16, fontWeight: "bold" }}>등록</Text>
+                </Pressable>
+                <Pressable onPress={onCloseExtend} style={{ padding: 10 }}>
+                  <Text style={{ fontSize: 16, fontWeight: "bold" }}>취소</Text>
+                </Pressable>
+              </View>
+            </View>
+          </View>
+        </Modal>
+        {membership?.status === "active" ? (
+          <Pressable
+            style={styles.button}
+            android_ripple={{ color: "#ededed" }}
+            onPress={onPressChange}
+          >
+            <Text>요일/시간변경</Text>
+          </Pressable>
+        ) : null}
+        <Modal
+          visible={showSecond}
+          animationType="fade"
+          transparent={true}
+          onRequestClose={onCloseChange}
         >
-          <Text>요일/시간변경</Text>
-        </Pressable>
+          <View style={styles.modalContainer}>
+            <View style={styles.modalContent}>
+              <View
+                style={{
+                  position: "absolute",
+                  top: 15,
+                  left: 22,
+                }}
+              >
+                <Text style={{ fontSize: 24 }}>회원 스케줄 입력</Text>
+              </View>
+              <View>
+                <View>
+                  {Object.entries(membershipDays).map(([day, data]) => (
+                    <View
+                      key={day}
+                      style={{ flexDirection: "row", alignItems: "center" }}
+                    >
+                      <Text>{day}</Text>
+                      <CheckBox
+                        checked={data.checked}
+                        onPress={() => {
+                          setMembershipDays((prevInfo) => ({
+                            ...prevInfo,
+                            [day]: {
+                              ...prevInfo[day],
+                              checked: !data.checked,
+                            },
+                          }));
+                        }}
+                      />
+                      <View
+                        style={{
+                          borderWidth: 1,
+                          borderRadius: 2,
+                          paddingHorizontal: 10,
+                          marginRight: 10,
+                          height: 24,
+                          width: 72,
+                          flexDirection: "row",
+                          alignItems: "center",
+                        }}
+                      >
+                        <TextInput
+                          keyboardType="numeric"
+                          value={data.startHours}
+                          maxLength={2}
+                          editable={data.checked}
+                          onChangeText={(text) => {
+                            setMembershipDays((prevInfo) => ({
+                              ...prevInfo,
+                              [day]: {
+                                ...prevInfo[day],
+                                startHours: text,
+                              },
+                            }));
+                          }}
+                        />
+                        <Text style={{ marginRight: 5 }}> : </Text>
+                        <TextInput
+                          keyboardType="numeric"
+                          value={data.startMinutes}
+                          maxLength={2}
+                          editable={data.checked}
+                          onChangeText={(text) => {
+                            setMembershipDays((prevInfo) => ({
+                              ...prevInfo,
+                              [day]: {
+                                ...prevInfo[day],
+                                startMinutes: text,
+                              },
+                            }));
+                          }}
+                        />
+                      </View>
+                      <Text style={{ marginRight: 10 }}>~</Text>
+                      <View
+                        style={{
+                          borderWidth: 1,
+                          borderRadius: 2,
+                          paddingHorizontal: 10,
+                          marginRight: 10,
+                          height: 24,
+                          width: 72,
+                          flexDirection: "row",
+                          alignItems: "center",
+                        }}
+                      >
+                        <TextInput
+                          keyboardType="numeric"
+                          value={data.endHours}
+                          maxLength={2}
+                          editable={data.checked}
+                          onChangeText={(text) => {
+                            setMembershipDays((prevInfo) => ({
+                              ...prevInfo,
+                              [day]: {
+                                ...prevInfo[day],
+                                endHours: text,
+                              },
+                            }));
+                          }}
+                        />
+                        <Text style={{ marginRight: 5 }}> : </Text>
+                        <TextInput
+                          keyboardType="numeric"
+                          value={data.endMinutes}
+                          maxLength={2}
+                          editable={data.checked}
+                          onChangeText={(text) => {
+                            setMembershipDays((prevInfo) => ({
+                              ...prevInfo,
+                              [day]: {
+                                ...prevInfo[day],
+                                endMinutes: text,
+                              },
+                            }));
+                          }}
+                        />
+                      </View>
+                    </View>
+                  ))}
+                </View>
+              </View>
+              <View
+                style={{
+                  flexDirection: "row",
+                  position: "absolute",
+                  bottom: 10,
+                  right: 10,
+                }}
+              >
+                <Pressable onPress={onSaveChange} style={{ padding: 10 }}>
+                  <Text style={{ fontSize: 16, fontWeight: "bold" }}>등록</Text>
+                </Pressable>
+                <Pressable onPress={onCloseChange} style={{ padding: 10 }}>
+                  <Text style={{ fontSize: 16, fontWeight: "bold" }}>취소</Text>
+                </Pressable>
+              </View>
+            </View>
+          </View>
+        </Modal>
       </View>
     </View>
   );
@@ -175,6 +494,20 @@ const styles = StyleSheet.create({
     backgroundColor: "white",
     paddingVertical: 16,
     paddingHorizontal: 24,
+  },
+  modalContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
+  },
+  modalContent: {
+    backgroundColor: "white",
+    paddingHorizontal: 20,
+    paddingVertical: 60,
+    borderRadius: 10,
+    width: "80%",
+    alignItems: "center",
   },
 });
 
