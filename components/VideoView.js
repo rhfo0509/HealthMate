d4fb72f3df36397502d3aefa19b80d70c3eca4f3
BEAR <ungnam0509@gmail.com>
2024-05-12 17:40:44 +0900

    feat: 동영상 재생 기능 구현

--- components/VideoView.js
+++ components/VideoView.js
@@ -1,53 +1,30 @@
 import { useRef, useState } from "react";
-import { View, StyleSheet, Button } from "react-native";
+import { useWindowDimensions } from "react-native";
 import { Video, ResizeMode } from "expo-av";
 
 function VideoView({ URL }) {
   const video = useRef(null);
   const [status, setStatus] = useState({});
+  const { width } = useWindowDimensions();
+
   return (
-    <View style={styles.container}>
-      <Video
-        ref={video}
-        style={styles.video}
-        source={{
-          uri: URL,
-        }}
-        useNativeControls
-        resizeMode={ResizeMode.CONTAIN}
-        isLooping
-        onPlaybackStatusUpdate={(status) => setStatus(() => status)}
-      />
-      <View style={styles.buttons}>
-        <Button
-          title={status.isPlaying ? "Pause" : "Play"}
-          onPress={() =>
-            status.isPlaying
-              ? video.current.pauseAsync()
-              : video.current.playAsync()
-          }
-        />
-      </View>
-    </View>
+    <Video
+      ref={video}
+      style={{
+        alignSelf: "center",
+        width: width,
+        height: (width / 16) * 9,
+        marginBottom: 16,
+      }}
+      source={{
+        uri: URL,
+      }}
+      useNativeControls={true}
+      isLooping={false}
+      resizeMode={ResizeMode.CONTAIN}
+      onPlaybackStatusUpdate={(status) => setStatus(() => status)}
+    />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    backgroundColor: "#ecf0f1",
-  },
-  video: {
-    alignSelf: "center",
-    width: 320,
-    height: 200,
-  },
-  buttons: {
-    flexDirection: "row",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-});
 
 export default VideoView;
